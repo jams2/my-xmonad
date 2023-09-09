98e74980d800eb3c35ce45bb464e89924a82b544
Joshua Munn <public@elysee-munn.family>
2023-09-09 13:06:08 +0100

    Updates

--- app/xmonad.hs
+++ app/xmonad.hs
@@ -5,16 +5,22 @@ import Data.Map qualified as Map
 import Elisp
 import Graphics.X11.ExtraTypes.XF86
 import XMonad
+import XMonad.Actions.CopyWindow (copy)
+import XMonad.Actions.CycleWS (toggleWS)
 import XMonad.Actions.Submap (submap)
-import XMonad.Hooks.DynamicLog (xmobar)
-import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
-import XMonad.Hooks.ManageDocks (avoidStruts)
-import XMonad.Hooks.ManageHelpers (doCenterFloat)
-import XMonad.Layout.Spacing (Border (..), spacingRaw)
+import XMonad.Config.Gnome (gnomeConfig)
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks (ToggleStruts (..), avoidStruts)
+import XMonad.Hooks.ManageHelpers (Side (CE), doCenterFloat, doSideFloat)
+import XMonad.Layout.Spacing
+import XMonad.Layout.Spiral (spiral)
+import XMonad.StackSet qualified as W
 import XMonad.Util.EZConfig (additionalKeys)
 
+home = "/home/jmunn/"
+projectDir = home ++ "projects/"
+orgDir = home ++ "org/"
 myTerminal = "wezterm"
-
 winKey = mod4Mask
 
 class ToArgString a where
@@ -58,16 +64,23 @@ orgAgendaArgs :: EmacsClientArgs
 orgAgendaArgs =
   emacsClientDefaults
     { emacsClientEval = orgAgendaEval,
-      emacsClientFrameParams = [elisp|((name . "*Org Agenda*"))|]
+      emacsClientFrameParams = [elisp|((name . "*My Org Agenda*") (width . 120))|]
     }
 
-orgCaptureEval = [elisp|(org-capture nil "g")|]
+orgCaptureActionEval = [elisp|(org-capture nil "g")|]
 
 orgCaptureActionArgs :: EmacsClientArgs
 orgCaptureActionArgs =
   emacsClientDefaults
-    { emacsClientEval = orgCaptureEval,
-      emacsClientFrameParams = [elisp|((name . "*My Org Capture*"))|]
+    { emacsClientEval = orgCaptureActionEval,
+      emacsClientFrameParams = [elisp|((name . "*My Org Capture*") (width . 120))|]
+    }
+
+orgCaptureNoteArgs :: EmacsClientArgs
+orgCaptureNoteArgs =
+  emacsClientDefaults
+    { emacsClientEval = [elisp|(org-capture nil "n")|],
+      emacsClientFrameParams = [elisp|((name . "*My Org Capture*") (width . 120))|]
     }
 
 orgNextActionsCommand =
@@ -80,11 +93,14 @@ orgNextActionsArgs :: EmacsClientArgs
 orgNextActionsArgs =
   emacsClientDefaults
     { emacsClientEval = orgNextActionsCommand,
-      emacsClientFrameParams = [elisp|((name . "*My Org Capture*"))|]
+      emacsClientFrameParams = [elisp|((name . "*My Org Capture*") (width . 120))|]
     }
 
 orgViewGtdFileArgs :: EmacsClientArgs
-orgViewGtdFileArgs = emacsClientDefaults {emacsClientFile = "/home/joshua/org/gtd.org"}
+orgViewGtdFileArgs = emacsClientDefaults {emacsClientFile = orgDir ++ "gtd.org"}
+
+orgViewNotesArgs :: EmacsClientArgs
+orgViewNotesArgs = emacsClientDefaults {emacsClientFile = orgDir ++ "notes.org"}
 
 emacsCalcArgs :: EmacsClientArgs
 emacsCalcArgs =
@@ -95,13 +111,44 @@ emacsCalcArgs =
 
 emacsViewXmonadConfArgs :: EmacsClientArgs
 emacsViewXmonadConfArgs =
-  emacsClientDefaults {emacsClientFile = "/home/joshua/projects/my-xmonad/app/xmonad.hs"}
+  emacsClientDefaults {emacsClientFile = projectDir ++ "my-xmonad/app/xmonad.hs"}
+
+pythonScratchEval :: ElispExpr
+pythonScratchEval =
+  [elisp|
+(let ((file (make-temp-file "py_scratch" nil ".py" "#!/usr/bin/python")))
+  (find-file file))
+|]
+
+emacsPythonScratchBufferArgs :: EmacsClientArgs
+emacsPythonScratchBufferArgs =
+  emacsClientDefaults
+    { emacsClientEval = pythonScratchEval,
+      emacsClientFrameParams = [elisp|((name . "*Python Scratch*"))|]
+    }
+
+markDownScratchEval :: ElispExpr
+markDownScratchEval =
+  [elisp|
+(let ((file (make-temp-file "md_scratch" nil ".md")))
+  (find-file file))
+|]
+
+emacsMarkdownScratchBufferArgs :: EmacsClientArgs
+emacsMarkdownScratchBufferArgs =
+  emacsClientDefaults
+    { emacsClientEval = markDownScratchEval,
+      emacsClientFrameParams = [elisp|((name . "*MarkDown Scratch*"))|]
+    }
 
 myKeys =
   [ ( (winKey, xK_e),
       submap . Map.fromList $
         [ ( (0, xK_c),
             spawn $ getEmacsClientCommand emacsClientDefaults
+          ),
+          ( (0, xK_r),
+            spawn "systemctl restart --user emacs.service"
           ),
           ( (0, xK_x),
             spawn $ getEmacsClientCommand emacsViewXmonadConfArgs
@@ -115,21 +162,56 @@ myKeys =
           ( (0, xK_g),
             spawn $ getEmacsClientCommand orgViewGtdFileArgs
           ),
-          ( (0, xK_n),
-            spawn $ getEmacsClientCommand orgNextActionsArgs
-          ),
           ( (shiftMask, xK_g),
             spawn $ getEmacsClientCommand orgCaptureActionArgs
+          ),
+          ( (0, xK_l),
+            spawn $ getEmacsClientCommand orgNextActionsArgs
+          ),
+          ( (0, xK_n),
+            spawn $ getEmacsClientCommand orgViewNotesArgs
+          ),
+          ( (shiftMask, xK_n),
+            spawn $ getEmacsClientCommand orgCaptureNoteArgs
+          ),
+          ( (0, xK_p),
+            spawn $ getEmacsClientCommand emacsPythonScratchBufferArgs
+          ),
+          ( (0, xK_m),
+            spawn $ getEmacsClientCommand emacsMarkdownScratchBufferArgs
           )
         ]
     ),
+    ((winKey .|. shiftMask, xK_s), spawn "gnome-control-center"),
     -- Media keys
+    ((winKey, xK_F8), spawn "amixer -D pulse sset Master toggle"),
+    ((winKey, xK_F9), spawn "amixer -D pulse sset Master 10%-"),
+    ((winKey, xK_F10), spawn "amixer -D pulse sset Master 10%+"),
     ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 10%+"),
     ((0, xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 10%-"),
     ((0, xF86XK_AudioMute), spawn "amixer -D pulse sset Master toggle"),
     ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10"),
-    ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
+    ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10"),
+    ((winKey, xK_b), toggleMenuBarAndGaps),
+    ((winKey, xK_Tab), toggleWS),
+    ((winKey, xK_v), spawn "pavucontrol"),
+    ((winKey, xK_Print), spawn "flameshot gui")
   ]
+    -- mod 1-9 switch to workspace
+    -- mod-shift 1-9 move client to workspace
+    -- mod-control-shift 1-9 copy client to workspace
+    ++ [ ((winKey .|. m, k), windows $ f i)
+         | (i, k) <- zip myWorkspaces [xK_1 ..],
+           (f, m) <- [(W.view, 0), (W.shift, shiftMask), (copy, shiftMask .|. controlMask)]
+       ]
+
+toggleMenuBarAndGaps :: X ()
+toggleMenuBarAndGaps = do
+  toggleScreenSpacingEnabled
+  toggleWindowSpacingEnabled
+  sendMessage ToggleStruts
+
+myWorkspaces = map show ([1 .. 9] :: [Int])
 
 myNormalBorderColor = "#555"
 
@@ -139,12 +221,12 @@ myBorderWidth = 2
 
 myManageHook =
   composeAll
-    [ className =? "Mullvad VPN" --> doFloat <+> doShift "9",
-      className =? "Pavucontrol" --> doFloat,
-      className =? "vlc" --> doFloat,
-      className =? "Breach" --> doFloat,
-      className =? "Paradox Launcher" --> doFloat,
-      title =? "*Org Agenda*" --> doCenterFloat,
+    [ className =? "Pavucontrol" --> doCenterFloat,
+      className =? "Gnome-control-center" --> doCenterFloat,
+      className =? "Gnome-panel" <&&> title =? "Run Application" --> doCenterFloat,
+      className =? "Evolution-alarm-notify" --> doSideFloat CE,
+      className =? "zoom" --> doCenterFloat,
+      title =? "*My Org Agenda*" --> doCenterFloat,
       title =? "*My Org Capture*" --> doCenterFloat,
       title =? "*Calc*" --> doCenterFloat
     ]
@@ -152,23 +234,24 @@ myManageHook =
 myLayout =
   avoidStruts $
     spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True $
-      Tall nmaster delta ratio ||| Full
+      Tall nmaster delta ratio ||| Full ||| spiral (6 / 7)
   where
     nmaster = 1
     ratio = 1 / 2
     delta = 3 / 100
 
-myConfig =
-  def
-    { terminal = myTerminal,
-      modMask = winKey,
-      borderWidth = myBorderWidth,
-      normalBorderColor = myNormalBorderColor,
-      focusedBorderColor = myFocusedBorderColor,
-      manageHook = myManageHook <+> def,
-      layoutHook = myLayout
-    }
-    `additionalKeys` myKeys
-
 main :: IO ()
-main = xmonad =<< xmobar (ewmhFullscreen $ ewmh myConfig)
+main = do
+  xmonad $
+    ewmhFullscreen $
+      ewmh
+        gnomeConfig
+          { terminal = myTerminal,
+            modMask = winKey,
+            borderWidth = myBorderWidth,
+            focusedBorderColor = myFocusedBorderColor,
+            normalBorderColor = myNormalBorderColor,
+            manageHook = myManageHook <+> manageHook gnomeConfig,
+            layoutHook = myLayout
+          }
+        `additionalKeys` myKeys
